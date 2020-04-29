6f7bb541fc36a911024007beabf6c8c8154db893
Salman Nazir <salman.nazir@coyoapp.com>
2020-04-29 09:15:26 +0200

    - Snackbar - Implement Logout - Activity Lifecycle

--- app/src/main/java/theta/ramadan/android/HomeActivity.kt
+++ app/src/main/java/theta/ramadan/android/HomeActivity.kt
@@ -1,20 +1,25 @@
 package theta.ramadan.android
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
-import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import theta.ramadan.android.ui.main.SectionsPagerAdapter
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // setting UI Design
         setContentView(R.layout.activity_home)
+
+
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
         val viewPager: ViewPager = findViewById(R.id.view_pager)
         viewPager.adapter = sectionsPagerAdapter
@@ -23,8 +28,67 @@ class HomeActivity : AppCompatActivity() {
         val fab: FloatingActionButton = findViewById(R.id.fab)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "Logout Successfully", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
+
+            // On Logout, Move to Login Activity
+
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP
+            this.finish()
+            startActivity(intent)
         }
+
+
+        // Toast.makeText(this, "OnCreate", Toast.LENGTH_LONG).show()
+        Log.d("HomeActivity", "OnCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Attached UI Design come to the screen
+        // User can see the design on the screen
+        // User can't interact with the screen
+        // activity comes to Foreground
+//        Toast.makeText(this, "OnStart", Toast.LENGTH_LONG).show()
+        Log.d("HomeActivity", "onStart")
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+//        Toast.makeText(this, "OnResume", Toast.LENGTH_LONG).show()
+        Log.d("HomeActivity", "OnResume")
+
+        // UI on the screen now able to interact by user
+        // i.e. button click, scroll etc
+    }
+
+    override fun onPause() {
+        super.onPause()
+//        Toast.makeText(this, "OnPause", Toast.LENGTH_LONG).show()
+        Log.d("HomeActivity", "OnPause")
+
+        // partially out of screen
+        // No more interaction
+
+    //    if(someVoiceMessage.isPlaying())
+                    // voiceMessage.pause()
+        //OR
+        //song.keepPlaying()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("HomeActivity", "onStop")
+
+        // COmpletly out of screen
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("HomeActivity", "onDestroy")
+
     }
 }
